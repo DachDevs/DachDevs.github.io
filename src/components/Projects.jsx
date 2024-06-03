3370a53f8bbb8863c0da26d7a0e7d2d9c015c2c9
Henny Feliz <mingito.2017@gmail.com>
2024-06-03 01:06:57 -0400

    Projects component and css styles file

---
+++ src/components/Projects.jsx
@@ -0,0 +1,19 @@
+import '../styles/Projects.css'
+
+const Projects = () => {
+  return (
+    <div className='projects'>
+      <div className='project'>
+        <div className='project-content'>Content 1</div>
+      </div>
+      <div className='project'>
+        <div className='project-content'>Content 2</div>
+      </div>
+      <div className='project'>
+        <div className='project-content'>Content 3</div>
+      </div>
+    </div>
+  )
+}
+
+export default Projects
