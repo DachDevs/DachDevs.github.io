d899f33e92062f304e4e45b3ba60d5673742d0f9
Henny Feliz <mingito.2017@gmail.com>
2024-06-03 01:07:08 -0400

    navbar component and css styles file

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import '../styles/Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className="Navbar">
+      <span>Projects</span>
+      <span>About</span>
+      <span>Contact</span>
+    </nav>
+  )
+}
+
+export default Navbar
