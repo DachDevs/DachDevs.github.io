1573c48a083777a14cbb5076349a883c9db3daa8
Henny Feliz <mingito.2017@gmail.com>
2024-06-05 07:18:05 -0400

    Some new empy components for later

---
+++ src/components/Information.jsx
@@ -0,0 +1,9 @@
+import '../styles/Information.css'
+
+const Information = () => {
+  return (
+    <div>Information</div>
+  )
+}
+
+export default Information
---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import '../styles/Main.css'
+
+const Main = () => {
+  return (
+    <div>
+      {/* <h1>Hi, Im a Frontend Developer</h1>
+      <p>My name is John Doe and I am a frontend developer. I have been working in the industry for 5 years and I have experience in a variety of technologies including HTML, CSS, JavaScript, React, and more.</p>
+      <p>Feel free to check out my work below and get in touch if you would like to collaborate on a project.</p> */}
+    </div>
+  )
+}
+
+export default Main
